ee46107720697edfa2eeb8b7ca1776bc3c7fc495
meshete <91982262+meshete@users.noreply.github.com>
2022-04-02 00:47:58 -0400

    updated js
    added event listener. updated css and html to link to js.

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+    $("#tabs-1").hover(function() {
+        $(this).css("background-color", "grey");
+    }, function() {
+        $(this).css("background-color", "#373738");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#tabs-2").hover(function() {
+        $(this).css("background-color", "grey");
+    }, function() {
+      $(this).css("background-color", "#373738");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#tabs-3").hover(function() {
+        $(this).css("background-color", "grey");
+    }, function() {
+      $(this).css("background-color", "#373738");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#tabs-4").hover(function() {
+        $(this).css("background-color", "grey");
+    }, function() {
+      $(this).css("background-color", "#373738");
+    });
+  });
